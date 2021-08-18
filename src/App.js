0b417c2a762dcb201f3a5e742d9a3866ec929636
surajpshenoy <surajpshenoy@gmail.com>
2021-08-18 14:11:26 +1000

    Added the iinitial text inmput form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useState } from "react";
+//import Values from "value.js";
 
 function App() {
+  const [color, setColor] = useState();
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn">submit</button>
+        </form>
+      </section>
+      <section className="colors"></section>
+    </>
   );
 }
 
