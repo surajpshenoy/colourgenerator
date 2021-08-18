5abe4bac3e9fee68f8bcb43fffab143d616d9859
surajpshenoy <surajpshenoy@gmail.com>
2021-08-18 14:33:42 +1000

    function to retrieve the colors list from Value.js API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-//import Values from "value.js";
+import Values from "values.js"
+
 
 function App() {
   const [color, setColor] = useState();
@@ -8,6 +9,15 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+    console.log(colors);
+    }
+    catch(error) {
+      console.log(error);
+      setError(true)
+    }
+    
   };
 
   return (
@@ -20,6 +30,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
+            className = {`${error? 'error' : null}`}
           />
           <button className="btn">submit</button>
         </form>
