ce48c9b7c475f042277be5af3190764c9046e0ed
surajpshenoy <surajpshenoy@gmail.com>
2021-08-20 13:47:07 +1000

    fixed errors in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import Values from "values.js";
 
+
 function App() {
-  const [color, setColor] = useState();
-  const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    try {
-      let colors = new Values(color).all(10);
-      console.log(colors);
-    } catch (error) {
-      console.log(error);
-      setError(true);
-    }
-  };
+const [color, setColor] = useState();
+const [error, setError] = useState(false);
+const [list, setList] = useState([]);
 
-  return (
-    <>
-      <section className="container">
-        <h3>color generator</h3>
-        <form onSubmit={handleSubmit}>
-          <input
+const handleSubmit = (event) => {
+  event.preventDefault();
+  try {
+    let colors = new Values(color).all(10);
+    console.log(colors);
+  } catch (error) {
+    console.log(error);
+    setError(true);
+  }
+};
+
+return(
+  <div>
+    <section className="container">
+         <h3>color generator</h3>
+         <form onSubmit={handleSubmit}>
+           <input
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
@@ -33,8 +35,20 @@ function App() {
         </form>
       </section>
       <section className="colors"></section>
-    </>
-  );
+  </div>
+)
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
